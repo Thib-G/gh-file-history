a5c96ea3aef0fa8b656ae34640ea00d902281044
Thibaut Goelff <thibaut@example.com>
2023-07-07 10:14:00 +0200

    Add branch to match

--- app.py
+++ app.py
@@ -9,20 +9,23 @@ st.write("""
          
 file_url = st.text_input('Enter link to file on GitHub', value='https://github.com/Thib-G/pasevident.be/blob/main/src/assets/sentences/index.js')
 
-pattern = r'^https:\/\/github.com\/(.+)\/(.+)\/blob\/main\/(.+)$'
+pattern = r'^https:\/\/github.com\/(.+)\/(.+)\/blob\/(main|master)\/(.+)$'
 p = re.compile(pattern)
 matches = p.findall(file_url)
-owner, repo, path = matches[0]
+owner, repo, branch, path = matches[0]
 
 
 st.write(f"""
  - owner: {owner}
  - repo: {repo}
+ - branch: {branch}
  - path: {path}
 """)
 
 
-url = f'https://api.github.com/repos/{owner}/{repo}/commits?sha=main&path=/{path}'
+url = f'https://api.github.com/repos/{owner}/{repo}/commits?sha={branch}&path=/{path}'
+
+st.write(f'URL: `{url}`')
 
 r = requests.get(url)
 
