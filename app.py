1d72fe541d01c7b9fcbcb51336c497d8186d1d07
Thibaut Goelff <thibaut@example.com>
2023-07-13 20:52:08 +0200

    Update to add Readme and minor improvements

--- app.py
+++ app.py
@@ -3,29 +3,45 @@ import requests
 import pandas as pd
 import re
 
-st.write("""
-    # Extract commit history
+st.set_page_config(page_title='AI-pocratic oath')
+
+repo_url = 'https://github.com/virginiemarelli/AI-ethic-oath'
+
+st.markdown(f"""
+    Source: {repo_url}
 """)
+
+@st.cache_data
+def get_readme():
+    r = requests.get('https://raw.githubusercontent.com/virginiemarelli/AI-ethic-oath/main/README.md')
+    return r.text.replace('AIEthicOath.md', 'https://github.com/virginiemarelli/AI-ethic-oath/blob/main/AIEthicOath.md')
+
+
+st.write(get_readme())
          
-file_url = st.text_input('Enter link to repo on GitHub', value='https://github.com/virginiemarelli/AI-ethic-oath')
+
 
 pattern = r'^https:\/\/github.com\/(.+)\/(.+)'
 p = re.compile(pattern)
-matches = p.findall(file_url)
+matches = p.findall(repo_url)
 owner, repo = matches[0]
 
 branch = 'main'
 
 
-st.write(f"""
- - owner: {owner}
- - repo: {repo}
- - branch: {branch}
-""")
+# st.write(f"""
+# - owner: {owner}
+# - repo: {repo}
+# - branch: {branch}
+# """)
 
 
 url = f'https://api.github.com/repos/{owner}/{repo}/commits?sha=main'
 
+
+st.write("""
+## They have pledged:
+""")
 st.write(f'URL: `{url}`')
 
 r = requests.get(url)
@@ -34,11 +50,11 @@ r = requests.get(url)
 
 commits_flat = [
     {
-        'message': row.get('commit').get('message'),
-        'github author': row.get('author').get('login', '?') if row.get('author') else '?',
-        'commit author': row.get('commit').get('author').get('name'),
-        'date': row.get('commit').get('author').get('date'),
-        'html_url': row.get('html_url'),
+        'Message': row.get('commit').get('message'),
+        'Github author': row.get('author').get('login', '?') if row.get('author') else '?',
+        'Commit author': row.get('commit').get('author').get('name'),
+        'Date': row.get('commit').get('author').get('date'),
+        'Link': row.get('html_url'),
     }
     for row
     in r.json()
@@ -46,4 +62,9 @@ commits_flat = [
 
 df = pd.DataFrame(commits_flat)
 
-st.write(df)
+st.dataframe(
+    df[df['Message'] == 'I pledge'],
+    column_config={
+        'Link': st.column_config.LinkColumn()
+    }
+)
